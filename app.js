56222695c86b46b72164e8f52d284afd955dfc5f
Cylecq <leclercq.cyril06@gmail.com>
2022-11-02 16:28:31 +0100

    Challenge Done

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
-
 const express = require("express");
-
 const app = express();
-
 const port = process.env.APP_PORT ?? 5000;
+
+const movieHandlers = require("./movieHandlers");
+const usersHandlers = require("./usersHandlers");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
